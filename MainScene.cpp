4d0119f8f384d21e478089a3dce6866afbcff00c
lumpumpum <kjh04215@gmail.com>
2022-12-16 01:14:56 +0900

    승리판정 구현, 경기장아웃 구현

--- MainScene.cpp
+++ MainScene.cpp
@@ -6,9 +6,15 @@ using namespace std;
 
 
 #define winSize		500		// 창의 크기
-#define playerNum	5		// 플레이어 수
+#define playerNum	10		// 플레이어 수
 
 int corrWin = (winSize / 2); // 윈도우 사이즈에 따른 중앙 보정값
+
+// 남은 돌
+int aliveRed, aliveBlue;
+
+// 현재 턴
+char currentTurn = 'r';
 
 // 플레이어 설정
 int		selectedPlayer = -1; // 플레이어가 선택 되었는지 판단
@@ -50,13 +56,13 @@ GLfloat		vertices[][3] = {
 	{ boardWidth,  boardWidth, boardHeight },	// 6 아랫쪽 우상단
 	{ boardWidth, -boardWidth, boardHeight } };	// 7 아랫쪽 좌상단
 
-
 class Player
 {
 public:
 	GLfloat x, y;
 	GLfloat dirX = 0.0, dirY = 0.0;
 	GLfloat velocity;
+	bool alive = true;
 	char teamColor;
 };
 
@@ -68,11 +74,20 @@ bool IsCollision(int j, int k);
 // 초기설정
 void init(void)
 {
-	player[0].x = 0.0;		player[0].y = 0.0;		player[0].teamColor = 'r';
-	player[1].x = 50.0;		player[1].y = 50.0;		player[1].teamColor = 'r';
-	player[2].x = 100.0;	player[2].y = 100.0;	player[2].teamColor = 'r';
-	player[3].x = 150.0;	player[3].y = 150.0;	player[3].teamColor = 'b';
-	player[4].x = 200.0;	player[4].y = 200.0;	player[4].teamColor = 'b';
+	player[0].x = -160.0;		player[0].y = -180.0;		player[0].teamColor = 'r';
+	player[1].x = -80;			player[1].y = -180.0;		player[1].teamColor = 'r';
+	player[2].x = 0.0;			player[2].y = -180.0;		player[2].teamColor = 'r';
+	player[3].x = 80.0;			player[3].y = -180.0;		player[3].teamColor = 'r';
+	player[4].x = 160.0;;		player[4].y = -180.0;		player[4].teamColor = 'r';
+
+	player[5].x = -160.0;		player[5].y = 180.0;		player[5].teamColor = 'b';
+	player[6].x = -80;			player[6].y = 180.0;		player[6].teamColor = 'b';
+	player[7].x = 0.0;			player[7].y = 180.0;		player[7].teamColor = 'b';
+	player[8].x = 80.0;			player[8].y = 180.0;		player[8].teamColor = 'b';
+	player[9].x = 160.0;;		player[9].y = 180.0;		player[9].teamColor = 'b';
+
+	aliveRed = playerNum / 2;
+	aliveBlue = playerNum / 2;
 
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_NORMALIZE); //단위 법선 벡터
@@ -146,24 +161,6 @@ void drawGameBoard(void) {
 	polygon(5, 4, 0, 1);
 }
 
-void drawAxis(void) {
-
-	glBegin(GL_LINES);
-	glColor3f(1.0, 0.0, 0.0); // red color
-	glVertex3f(0.0, 0.0, 0.0);
-	glVertex3f(100.0, 0.0, 0.0);
-
-	glColor3f(0.0, 1.0, 0.0); // green color
-	glVertex3f(0.0, 0.0, 0.0);
-	glVertex3f(0.0, 100.0, 0.0);
-
-	glColor3f(0.0, 0.0, 1.0); // blue color
-	glVertex3f(0.0, 0.0, 0.0);
-	glVertex3f(0.0, 0.0, 100.0);
-	glEnd();
-}
-
-
 // 화면의 비율 유지
 void reshape(int w, int h)
 {
@@ -186,6 +183,51 @@ void cameraSetting(void)
 	gluLookAt(0.0, 0.0, cameraZoom, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 }
 
+void renderbitmap(float x, float y, void* font, char* string) {
+	char* c;
+	glRasterPos2f(x, y);
+	for (c = string; *c != '\0'; c++) {
+		glutBitmapCharacter(font, *c);
+	}
+}
+
+void DrawText(void)
+{
+	char buf[100] = { 0 };
+	glColor3f(0.0, 0.0, 0.0);
+	sprintf_s(buf, "Current Turn :");
+	renderbitmap(-220, -220, GLUT_BITMAP_TIMES_ROMAN_24, buf);
+
+	if (currentTurn == 'r')
+	{
+		glColor3f(1.0, 0.0, 0.0);
+		sprintf_s(buf, "Red");
+		renderbitmap(-70, -220, GLUT_BITMAP_TIMES_ROMAN_24, buf);
+	}
+
+	else if (currentTurn == 'b')
+	{
+		glColor3f(0.0, 0.0, 1.0);
+		sprintf_s(buf, "Blue");
+		renderbitmap(-70, -220, GLUT_BITMAP_TIMES_ROMAN_24, buf);
+	}
+
+	// 승리 판정
+	if (aliveBlue == 0)
+	{
+		glColor3f(1.0, 0.0, 0.0);
+		sprintf_s(buf, "Win RED");
+		renderbitmap(0, 0, GLUT_BITMAP_TIMES_ROMAN_24, buf);
+	}
+	else if (aliveRed == 0)
+	{
+		glColor3f(0.0, 0.0, 1.0);
+		sprintf_s(buf, "Win Blue");
+		renderbitmap(0, 0, GLUT_BITMAP_TIMES_ROMAN_24, buf);
+	}
+
+}
+
 // 화면 그리기
 void display(void)
 {
@@ -194,7 +236,7 @@ void display(void)
 
 	cameraSetting();
 
-	drawAxis();
+	DrawText();
 	drawGameBoard();
 
 	for (int i = 0; i < playerNum; i++)
@@ -203,6 +245,27 @@ void display(void)
 	RunPhysics();
 
 	glFlush();
+}
+
+void OutCheck(int i)
+{
+	if ((player[i].x < -boardWidth || player[i].x > boardWidth) && (player[i].alive == true))
+	{
+		player[i].x *= 10;
+		player[i].alive = false;
+		if (player[i].teamColor == 'r') aliveRed -= 1;
+		else if (player[i].teamColor == 'b') aliveBlue -= 1;
+	}
+
+	else if ((player[i].y < -boardWidth || player[i].y > boardWidth) && (player[i].alive == true))
+	{
+		player[i].y *= 10;
+		player[i].alive = false;
+		if (player[i].teamColor == 'r') aliveRed -= 1;
+		else if (player[i].teamColor == 'b') aliveBlue -= 1;
+	}
+
+
 }
 
 void RunPhysics()
@@ -236,6 +299,8 @@ void RunPhysics()
 
 		if (player[i].velocity < 0.0)
 			player[i].velocity = 0.0;
+
+		OutCheck(i); // 플레이어가 경기장 밖으로 나갔는가?
 
 		glutPostRedisplay();
 
@@ -295,8 +360,8 @@ void mouseEvent(GLint Button, GLint State, GLint X, GLint Y)
 		// 예외처리
 		if (selectedPlayer == -1)
 			return;
-
-		cout << "성공" << endl;
+		if (currentTurn != player[selectedPlayer].teamColor)
+			return;
 
 		// 마우스 땐 위치
 		mouseX = X;
@@ -315,12 +380,15 @@ void mouseEvent(GLint Button, GLint State, GLint X, GLint Y)
 		squareDirY = player[selectedPlayer].dirY * player[selectedPlayer].dirY;
 	
 		player[selectedPlayer].velocity = sqrt(squareDirX + squareDirY);
-		if (player[selectedPlayer].velocity > 100.0)	player[selectedPlayer].velocity = 100.0;
-
-		cout << player[selectedPlayer].velocity << endl;
+		if (player[selectedPlayer].velocity > 120.0)	player[selectedPlayer].velocity = 120.0;
 
 		glutPostRedisplay();
 		selectedPlayer = -1;		// 예외조건 초기화
+
+		// 턴 넘김
+
+		if (currentTurn == 'r')		currentTurn = 'b';
+		else if (currentTurn == 'b')		currentTurn = 'r';
 
 	}
 }
